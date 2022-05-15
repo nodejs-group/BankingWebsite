10fc0c810e97ef369c645254e84e7d80d8b18d87
Prem Garg <gprem062@gmail.com>
2022-05-15 11:55:58 +0530

    Add user controller
    User routes & login functionality add
    User schema add to database

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const userRouter = require('./routes/userRouter')
 const db = require('./config/mongoConfig')
 const app = express()
 
 const PORT = 4000
 
+app.set('view engine','ejs')
+app.set('views','templates')
+
+
+app.use(express.json())
+
+
+app.use(userRouter)
 
 
 
